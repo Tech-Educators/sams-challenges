11b76f30b30f997a043369587f6095d4f51d6a11
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-02-05 11:48:13 +0000

    add challenges

---
+++ bubble-sort/app.js
@@ -0,0 +1,5 @@
+// hard
+// implement a bubble sort for an array of numbers
+
+// https://en.wikipedia.org/wiki/Bubble_sort
+
---
+++ dont-acutally-do-this-but-maybe-youre-interested/app.js
@@ -0,0 +1,17 @@
+/* implement a deterministic finite automaton to find out if a given string ends in 'ab'. You'll need to research statemachines. 
+
+You can assume the input is a string of only a or b. 
+
+// abb // false
+// aabab // true
+// ab // true
+// baabaa // false
+// aabab // true. 
+
+https://en.wikipedia.org/wiki/Deterministic_finite_automaton
+
+https://en.wikipedia.org/wiki/Finite-state_machine */
+
+// I put this in more because I'm learning about them right now, and this is very abstract. But you may find it interesting!
+
+// message me (sam) on discord if you're attempting this and want a hand. 
